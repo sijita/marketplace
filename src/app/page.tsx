710f152653aa0d3fd7f960a74ec89e0200f25047
sijita-leancore <65623835+sijita@users.noreply.github.com>
2025-02-21 23:50:48 -0500

    feat: unification of components on the home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,19 @@
+import Footer from '@/components/footer';
+import Hero from '@/components/hero';
+import PopularCategories from '@/components/popular-categories';
+import PopularProducts from '@/components/popular-products';
+import RandomProduct from '@/components/random-product';
+
 export default function Home() {
-  return <></>;
+  return (
+    <main className="container mx-auto px-4 py-8 flex flex-col gap-16">
+      <div className="grid gap-8 md:grid-cols-[2fr_1fr] h-96">
+        <Hero />
+        <RandomProduct />
+      </div>
+      <PopularProducts />
+      <PopularCategories />
+      <Footer />
+    </main>
+  );
 }
