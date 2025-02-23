7a9d6106fd7e3bbdf14a8b3728792cfd15910b71
sijita-leancore <65623835+sijita@users.noreply.github.com>
2025-02-22 20:37:03 -0500

    feat: add error page

---
+++ src/app/error.tsx
@@ -0,0 +1,17 @@
+'use client';
+import { Button } from '@/components/ui/button';
+
+export default function Error({ reset }: { reset: () => void }) {
+  return (
+    <div className="min-h-screen flex flex-col gap-5 items-center justify-center">
+      <h2 className="text-3xl font-bold">Hubo un error, intentelo de nuevo</h2>
+      <Button
+        className="text-black font-medium"
+        color="primary"
+        onClick={() => reset()}
+      >
+        Recargar
+      </Button>
+    </div>
+  );
+}
