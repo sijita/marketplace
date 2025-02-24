3de4222477116127a0e767ab79b6ca705786edf3
sijita-leancore <65623835+sijita@users.noreply.github.com>
2025-02-23 22:01:06 -0500

    feat: add responsive to sell page

--- src/app/(auth)/sell/page.tsx
+++ src/app/(auth)/sell/page.tsx
@@ -26,7 +26,7 @@ export default async function Page() {
     .select('id, name');
 
   return (
-    <div className="container mx-auto py-10 flex flex-col gap-10">
+    <div className="container mx-auto p-10 flex flex-col gap-10">
       <div className="flex flex-col gap-2">
         <h2 className="text-lg font-bold">Publica tu producto</h2>
         <p className="text-sm text-muted-foreground">
