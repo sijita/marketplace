2e3db0c3c1ea9d6e450fbc07e9b7e778377483b3
sijita <65623835+sijita@users.noreply.github.com>
2025-02-25 22:12:31 -0500

    feat: make navbar responsive

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
 import Header from '@/components/header';
 import Providers from './providers';
+import { createClient } from '@/utils/supabase/server';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -19,18 +20,24 @@ export const metadata: Metadata = {
   description: 'A marketplace for buying and selling all kinds of goods.',
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const supabase = await createClient();
+
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
   return (
     <html lang="es">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <Providers>
-          <Header />
+          <Header user={user} />
           {children}
         </Providers>
       </body>
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,7 +1,15 @@
+'use client';
 import Link from 'next/link';
 import { Button } from './ui/button';
-import { Heart, MapPinned, Search, ShoppingCart, User } from 'lucide-react';
-import { createClient } from '@/utils/supabase/server';
+import {
+  Heart,
+  MapPinned,
+  Menu,
+  Search,
+  ShoppingCart,
+  User as UserIcon,
+  X,
+} from 'lucide-react';
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -10,21 +18,24 @@ import {
   DropdownMenuTrigger,
 } from '@/components/ui/dropdown-menu';
 import SignoutButton from './signout-button';
+import { useState } from 'react';
+import { User } from '@supabase/supabase-js';
 
-export default async function Header() {
-  const supabase = await createClient();
-
-  const {
-    data: { user },
-  } = await supabase.auth.getUser();
+export default function Header({ user }: { user: User | null }) {
+  const [menuOpen, setMenuOpen] = useState(false);
 
   return (
-    <header className="border-b">
+    <nav className="border-b">
       <div className="container mx-auto flex h-16 items-center justify-between px-4">
-        <nav className="flex items-center space-x-6">
+        <div className="flex items-center">
           <Button className="rounded-lg" variant="ghost" size="icon">
             <MapPinned className="h-5 w-5" />
           </Button>
+          <Link href="/" className="text-xl font-bold ml-4">
+            Marketplace
+          </Link>
+        </div>
+        <div className="hidden md:flex items-center space-x-6">
           <Link href="/" className="text-sm font-medium">
             Inicio
           </Link>
@@ -34,11 +45,8 @@ export default async function Header() {
           <Link href="/sell" className="text-sm font-medium">
             Vender
           </Link>
-        </nav>
-        <Link href="/" className="text-xl font-bold">
-          Marketplace
-        </Link>
-        <div className="flex items-center space-x-4">
+        </div>
+        <div className="hidden md:flex items-center space-x-4">
           <Button className="rounded-lg" variant="ghost" size="icon">
             <Search className="h-5 w-5" />
           </Button>
@@ -53,7 +61,7 @@ export default async function Header() {
             <DropdownMenu>
               <DropdownMenuTrigger asChild>
                 <Button variant="default" size="icon">
-                  <User className="h-5 w-5" />
+                  <UserIcon className="h-5 w-5" />
                 </Button>
               </DropdownMenuTrigger>
               <DropdownMenuContent>
@@ -65,12 +73,81 @@ export default async function Header() {
           ) : (
             <Link href="/login" className="rounded-lg">
               <Button variant="ghost" size="icon">
-                <User className="h-5 w-5" />
+                <UserIcon className="h-5 w-5" />
               </Button>
             </Link>
           )}
         </div>
+        <div className="md:hidden">
+          <Button
+            className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700"
+            onClick={() => setMenuOpen(!menuOpen)}
+          >
+            <span className="sr-only">Open main menu</span>
+            {menuOpen ? (
+              <X className="block h-6 w-6" aria-hidden="true" />
+            ) : (
+              <Menu className="block h-6 w-6" aria-hidden="true" />
+            )}
+          </Button>
+        </div>
       </div>
-    </header>
+
+      {menuOpen && (
+        <div className="md:hidden">
+          <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
+            <Link
+              href="/"
+              className="block px-3 py-2 rounded-md text-base font-medium text-center hover:bg-slate-100"
+            >
+              Inicio
+            </Link>
+            <Link
+              href="#"
+              className="block px-3 py-2 rounded-md text-base font-medium text-center hover:bg-slate-100"
+            >
+              Comprar
+            </Link>
+            <Link
+              href="/sell"
+              className="block px-3 py-2 rounded-md text-base font-medium text-center hover:bg-slate-100"
+            >
+              Vender
+            </Link>
+            <div className="flex items-center justify-center space-x-4 px-3 py-2">
+              <Button className="rounded-lg" variant="ghost" size="icon">
+                <Search className="h-5 w-5" />
+              </Button>
+              <Button className="rounded-lg" variant="ghost" size="icon">
+                <Heart className="h-5 w-5" />
+              </Button>
+              <Button className="rounded-lg" variant="ghost" size="icon">
+                <ShoppingCart className="h-5 w-5" />
+              </Button>
+              {user ? (
+                <DropdownMenu>
+                  <DropdownMenuTrigger asChild>
+                    <Button variant="default" size="icon">
+                      <UserIcon className="h-5 w-5" />
+                    </Button>
+                  </DropdownMenuTrigger>
+                  <DropdownMenuContent>
+                    <DropdownMenuLabel>{user.email}</DropdownMenuLabel>
+                    <DropdownMenuSeparator />
+                    <SignoutButton />
+                  </DropdownMenuContent>
+                </DropdownMenu>
+              ) : (
+                <Link href="/login" className="rounded-lg">
+                  <Button variant="ghost" size="icon">
+                    <UserIcon className="h-5 w-5" />
+                  </Button>
+                </Link>
+              )}
+            </div>
+          </div>
+        </div>
+      )}
+    </nav>
   );
 }
